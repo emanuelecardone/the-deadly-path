f735e35715d217a4f1542b9e83af74c5ca01a12e
emanuelecardone <emanuele.cardone@appfactory.it>
2023-01-11 14:31:10 +0100

    Reuploaded

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- Button di bootstrap, verrà usato in tutte le sezioni quindi lo uso come componente -->
+    <button class="game_button btn btn-outline-success" type="button">{{msg}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+@import '../style/mixins.scss';
+
+    .game_button{
+        @include game_button;
+    }
+</style>
